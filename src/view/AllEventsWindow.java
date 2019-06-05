303d373345fe54a17d23d1ec355b062e7b65b4b1
Jakub Bogdan <kubab1998@o2.pl>
2019-06-05 11:17:07 +0200

    Filtering events

--- src/view/AllEventsWindow.java
+++ src/view/AllEventsWindow.java
@@ -11,10 +11,11 @@ import data.EventCollection;
 public class AllEventsWindow
 {
 	JFrame frame;
-	Vector<String> eventNames;
+	Vector<String> eventDetails, eventToFilter;
 	JScrollPane scroll;
 	JTextArea eventsArea;
-	JButton close;
+	JButton close, OK;
+	TextField filterField;
 	
 	AllEventsWindow(EventCollection events)
 	{
@@ -23,15 +24,22 @@ public class AllEventsWindow
 		frame.getContentPane().setLayout(null);
 		frame.setResizable(false);
 		
-		eventNames = new Vector<String>();
+		eventDetails = new Vector<String>();
+		eventToFilter = new Vector<String>();
 		
 		for (int i=0; i<events.getEvents().size(); i++)
 		{
-			eventNames.add(
+			eventDetails.add(
 			"Title: " + events.getEvent(i).getTitle() + "\nDescription: " + events.getEvent(i).getDescription() +
 			"\nPlace: " + events.getEvent(i).getPlace() + "\nStarting: " + MyEvent.displayDate(events.getEvent(i).getStartDate()) +
 			"\nEnding: " + MyEvent.displayDate(events.getEvent(i).getEndDate()));
+			
+			
+			eventToFilter.add(
+			events.getEvent(i).getTitle() + events.getEvent(i).getDescription() +
+			events.getEvent(i).getPlace());
 		}
+
 		
 		eventsArea = new JTextArea();
 		eventsArea.setBounds(20, 70, 400, 440);
@@ -41,18 +49,29 @@ public class AllEventsWindow
 		eventsArea.setFocusable(false);
 		eventsArea.scrollRectToVisible(new Rectangle(20,100));
 		
-		for (int i=0; i<eventNames.size(); i++)
-			eventsArea.append(eventNames.get(i) + "\n\n");
+		for (int i=0; i<eventDetails.size(); i++)
+			eventsArea.append(eventDetails.get(i) + "\n\n");
 		
 		scroll = new JScrollPane(eventsArea);
 		scroll.setBounds(20, 70, 400, 440);
 		scroll.setVisible(true);
 		frame.getContentPane().add(scroll);
 		
-		close = new JButton("Close");
-		close.setBounds(170, 520, 100, 30);
-		close.setFont(new Font("Times New Roman", Font.BOLD, 18));
-		frame.getContentPane().add(close);
+		addButtons();
+		
+		filterField = new TextField("Search events");
+		filterField.setBounds(20, 20, 160, 30);
+		filterField.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		frame.getContentPane().add(filterField);
+		
+		filterField.addMouseListener(new AdaptMouseListener()
+		{
+			@Override
+			public void mouseClicked(MouseEvent e) 
+			{
+				filterField.setText("");
+			}
+		});
 		
 		close.addActionListener(new ActionListener()
 		{
@@ -62,5 +81,34 @@ public class AllEventsWindow
 				frame.dispose();
 			}
 		});
+		
+		OK.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				eventsArea.setText("");
+				for (int i=0; i<eventToFilter.size(); i++)
+				{
+					if (eventToFilter.get(i).toLowerCase().contains(filterField.getText().toLowerCase()))
+						{
+							eventsArea.append(eventDetails.get(i) + "\n\n");
+						}
+				}
+			}
+		});
+	}
+	
+	private void addButtons()
+	{
+		close = new JButton("Close");
+		close.setBounds(170, 520, 100, 30);
+		close.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		frame.getContentPane().add(close);
+		
+		OK = new JButton("OK");
+		OK.setBounds(185, 20, 55, 30);
+		OK.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		frame.getContentPane().add(OK);
 	}
 }
